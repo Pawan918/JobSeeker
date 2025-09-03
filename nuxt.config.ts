e87ba047df1aad440d8e95cb23f2f4939aa71450
Pawan918 <pawanshah84489@gmail.com>
2025-09-03 23:19:40 +0530

    added: base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:5000/api'
+      apiBase: process.env.API_BASE_URL
     }
   },
   modules: ['nuxt-zod-i18n', '@nuxtjs/i18n', 'nuxt-toast'],
